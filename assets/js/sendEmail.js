cfac1db5f2eaec4b0bbf97dcfd183d3335fa0cbe
FraL96 <francesca_lupu@live.it>
2021-01-14 21:23:59 +0000

    On hobbies.html have been added pictures with descriptions and in skills.html have been added the skills

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,9 +7,14 @@ function sendMail (contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            
         },
         function(error) {
             console.log("FAILED", error);
         });
         return false;
-}
+    }   
+
+
+
+ 
