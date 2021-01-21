0503c629fa99361817073a5eae4ae8e847470826
FraL96 <francesca_lupu@live.it>
2021-01-21 15:31:13 +0000

    Added infowindows to the maps in education.html and skills.html

--- assets/js/map-edu.js
+++ assets/js/map-edu.js
@@ -1,27 +1,65 @@
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 48.88657528060063,
-            lng: 5.58210075595233
-        }
-    });
+//Markers
+
+var markers = [
+    {
+        "title": 'Code Institute',
+        "lat": '53.298222680724514',
+        "lng": '-6.178762161070416',
+        "description": '<b>Code Institute</b> <br>Educational Institution<br><br> <b>Address:</b> Blackrock Business Park, 1 Block 1, Carysfort Ave, Blackrock, Co. Dublin <br> <b>Phone:</b> +35315397973'
+    },
+    {
+        "title": 'NHL Stenden',
+        "lat": '53.21123019095071',
+        "lng": '5.797379697820648',
+        "description": '<b>NHL Stenden University of Applied Sciences</b> <br>University<br><br> <b>Address:</b> Rengerslaan 8, 8917 DD Leeuwarden, Netherlands <br> <b>Phone:</b> +31582441441'
+    },
+    {
+        "title": 'High School',
+        "lat": '41.804587477521295',
+        "lng": '13.222443497286879',
+        "description": '<b>IPSSEOA "Michelangelo Buonarroti" </b> <br>High School<br><br> <b>Address:</b> Via Giuseppe Garibaldi, 1, 03014 Fiuggi FR, Italy <br> <b>Phone:</b> +390775515669'
+    }
+    ];
+
+    //Labels of the markers
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var locations = [
-        { lat: 53.298222680724514, lng: -6.178762161070416},
-        { lat: 53.21123019095071, lng: 5.797379697820648},
-        { lat: 41.804587477521295, lng: 13.222443497286879}
-    ];
+    //Start Map + zoom + map type
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
-    
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+    window.onload = function () {
+        LoadMap();
+    }
+    function LoadMap() {
+        var mapOptions = {
+            center: new google.maps.LatLng(50.42678061004576, 5.9738709394920715),
+            zoom: 3,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        //InfoWindow
+
+        var infoWindow = new google.maps.InfoWindow();
+ 
+        for (var i = 0; i < markers.length; i++) {
+            var data = markers[i];
+            var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                label: labels[i % labels.length],
+                title: data.title
+            });
+ 
+            //Click event
+
+            (function (marker, data) {
+                google.maps.event.addListener(marker, "click", function() {
+                    infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+                    infoWindow.open(map, marker);
+                });
+            })(marker, data);
+        }
+    }
--- assets/js/map-we.js
+++ assets/js/map-we.js
@@ -1,26 +1,65 @@
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: {
-            lat: 51.508065748434404, 
-            lng: -0.12798484699652599
-        }
-    });
+//Markers
+
+var markers = [
+    {
+        "title": 'The George Hotel',
+        "lat": '52.66234492672294',
+        "lng": '-8.628624673982813',
+        "description": '<b>The George Hotel</b> <br>4-stars hotel<br><br> <b>Address:</b> Shannon St, Limerick<br> <b>Phone:</b> +35361460400'
+    },
+    {
+        "title": 'Dolce La Hulpe',
+        "lat": '50.74546059189099',
+        "lng": '4.455113141877488',
+        "description": '<b>Dolce By Wyndham La Hulpe Brussels</b> <br>4-stars hotel<br><br> <b>Address:</b>Chaussée de Bruxelles 135, 1310 La Hulpe, Belgium<br> <b>Phone:</b> +3222909800'
+    },
+    {
+        "title": 'NHL Stenden',
+        "lat": '53.21123019095071',
+        "lng": '5.797379697820648',
+        "description": '<b>NHL Stenden University of Applied Sciences</b> <br>University<br><br> <b>Address:</b> Rengerslaan 8, 8917 DD Leeuwarden, Netherlands <br> <b>Phone:</b> +31582441441'
+    }
+    ];
+
+//Labels of the markers
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var locations = [
-        { lat: 53.21123019095071, lng: 5.797379697820648},
-        { lat: 50.74545889467894, lng: 4.455060838801693},
-        { lat: 52.66235102718274, lng: -8.62864546110268}
-    ];
+    //Start Map + zoom + map type
 
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
+    window.onload = function () {
+        LoadMap();
+    }
+    function LoadMap() {
+        var mapOptions = {
+            center: new google.maps.LatLng(51.5240359482379, -0.13184722797831577),
+            zoom: 3,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        //InfoWindow
+
+        var infoWindow = new google.maps.InfoWindow();
+ 
+        for (var i = 0; i < markers.length; i++) {
+            var data = markers[i];
+            var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                label: labels[i % labels.length],
+                title: data.title
+            });
+ 
+            //Click event
+
+            (function (marker, data) {
+                google.maps.event.addListener(marker, "click", function() {
+                    infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
+                    infoWindow.open(map, marker);
+                });
+            })(marker, data);
+        }
+    }
